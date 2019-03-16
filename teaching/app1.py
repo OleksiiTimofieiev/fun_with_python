3a9f51ad482080142d301add0d4358cef9f20690
contrbas@gmail.com <K4rmU78B>
2019-03-16 17:41:04 +0200

    dictionary is finished

--- teaching/app1.py
+++ teaching/app1.py
@@ -1,21 +1,33 @@
 import json
 from difflib import get_close_matches
 
- # library index HTML => list of STL
-
-data = json.load(open("data.json", 'r'));
+data = json.load(open("data.json"))
 
 def translate(w):
     w = w.lower()
     if w in data:
-        return data[w];
+        return data[w]
+    elif w.title() in data:
+        return data[w.title()]
+    elif w.upper() in data: #in case user enters words like USA or NATO
+        return data[w.upper()]
     elif len(get_close_matches(w, data.keys())) > 0:
-        yn = input("Did you mean %s instead ? Enter Y if yes, or N for No\n" % get_close_matches(w, data.keys())[0])
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
         if yn == "Y":
             return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
     else:
-        return "The word doen`t exist."
+        return "The word doesn't exist. Please double check it."
 
-word = input("Enter the word: ");
+word = input("Enter word: ")
 
-print(translate(word));
+output = translate(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
