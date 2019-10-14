470380379377b175f5b0ceaf3a8c80877d16393c
contrbas@gmail.com <K4rmU78B>
2019-10-14 15:21:14 +0300

    Hello World

---
+++ Hello.py
@@ -0,0 +1,8 @@
+# This program says hello and ask for my name
+
+print('Hello World')
+print('What is my name ?')
+myName = input()
+print('It is good to meet you' + myName)
+print(len(myName))
+print(float(len(myName)))
